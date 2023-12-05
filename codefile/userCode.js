2f2e0a0c2b741328247707945bfc2413b3f6a5c1
Sagar Jaswal <80746702+sj9102001@users.noreply.github.com>
2023-12-05 12:27:38 +0530

    Delete codefile directory

--- codefile/userCode.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello, World!");
-var x = {
-  first_name: "Sagar",
-  last_name: "Jaswal"
-}
-
-console.log(x.first_name, x.last_name);
--- codefile/userCode.py
+++
@@ -1,3 +0,0 @@
-print("Hello, World!")
-for i in range(1,10):
-  print(i)
